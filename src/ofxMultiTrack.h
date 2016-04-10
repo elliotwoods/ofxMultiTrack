0399d3f238d00a326f5b8a98c705f848b5562a28
Elliot Woods <elliot@kimchiandchips.com>
2016-04-11 01:58:42 +0900

    Constants

--- src/ofxMultiTrack.h
+++ src/ofxMultiTrack.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "ofxMultiTrack/Constants.h"
 #include "ofxMultiTrack/Frame.h"
 #include "ofxMultiTrack/Receiver.h"
 #include "ofxMultiTrack/Sender.h"
---
+++ src/ofxMultiTrack/Constants.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace ofxMultiTrack {
+	enum Ports {
+		NodeControl = 2146,
+		ServerControl = 2046,
+		NodeToServerDataRangeBegin = 2050
+	};
+}
