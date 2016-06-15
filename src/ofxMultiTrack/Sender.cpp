dd57b0ce19a8a4797173fa19e674c72ea4bd0f24
Elie Zananiri <prisonerjohn@gmail.com>
2016-06-15 12:47:19 -0400

    Added SenderExtColor class.

--- src/ofxMultiTrack/Sender.cpp
+++ src/ofxMultiTrack/Sender.cpp
@@ -48,4 +48,77 @@ namespace ofxMultiTrack {
 	float Sender::getDeviceFrameRate() const {
 		return this->kinectFrameRateCounter.getFrameRate();
 	}
+
+#pragma mark SenderExtColor
+	//----------
+	void SenderExtColor::init(shared_ptr<ofxKinectForWindows2::Device> kinect, shared_ptr<ofxMachineVision::Grabber::Simple> grabber, string ipAddress, int port) {
+		this->kinect = kinect;
+		this->grabber = grabber;
+
+		this->sender.init(ipAddress, port);
+		this->comboFrame.init(this->kinect, this->grabber);
+	}
+
+	//----------
+	bool SenderExtColor::update() {
+		if (!this->kinect) {
+			ofLogWarning("ofxMultiTrack::SenderExtColor") << "Cannot update Sender until you initialise with a valid Kinect";
+		}
+		if (!this->grabber) {
+			ofLogWarning("ofxMultiTrack::SenderExtColor") << "Cannot update Sender until you initialise with a valid Grabber";
+		}
+
+		auto isFrameNew = false;
+
+		//update Kinect
+		{
+			this->kinectFrameRateCounter.update();
+
+			if (this->kinect->isFrameNew()) {
+				this->kinectFrameRateCounter.addFrame();
+
+				this->comboFrame.copyFromKinect();
+				isFrameNew = true;
+			}
+		}
+
+		//update grabber, if required
+		if (this->grabber)
+		{
+			this->grabberFrameRateCounter.update();
+
+			if (this->grabber->isFrameNew()) {
+				this->grabberFrameRateCounter.addFrame();
+
+				this->comboFrame.copyFromGrabber();
+				isFrameNew = true;
+			}
+		}
+
+		//send frame if new
+		if (isFrameNew) {
+			auto sentSuccess = this->sender.send(this->comboFrame.getMessage());
+
+			if (!sentSuccess) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	//----------
+	void SenderExtColor::setCameraParams(const vector<float> & distortion, const ofMatrix4x4 & view, const ofMatrix4x4 & projection) {
+		this->comboFrame.setCameraParams(distortion, view, projection);
+	}
+
+	//----------
+	shared_ptr<ofxMachineVision::Grabber::Simple> SenderExtColor::getGrabber() {
+		return this->grabber;
+	}
+
+	//----------
+	float SenderExtColor::getGrabberFrameRate() const {
+		return this->grabberFrameRateCounter.getFrameRate();
+	}
 }
--- src/ofxMultiTrack/Sender.h
+++ src/ofxMultiTrack/Sender.h
@@ -9,7 +9,7 @@ namespace ofxMultiTrack {
 	class Sender {
 	public:
 		void init(shared_ptr<ofxKinectForWindows2::Device>, string ipAddress, int port);
-		bool update(); // return false if dropped a frame
+		virtual bool update(); // return false if dropped a frame
 
 		shared_ptr<ofxKinectForWindows2::Device> getKinect();
 		ofxSquashBuddies::Sender & getSender();
@@ -23,5 +23,24 @@ namespace ofxMultiTrack {
 		DeviceFrame deviceFrame;
 
 		ofxSquashBuddies::Utils::FramerateCounter kinectFrameRateCounter;
+	};
+
+	class SenderExtColor : public Sender {
+	public:
+		void init(shared_ptr<ofxKinectForWindows2::Device>, shared_ptr<ofxMachineVision::Grabber::Simple>, string ipAddress, int port);
+		virtual bool update() override;
+
+		void setCameraParams(const vector<float> & distortion, const ofMatrix4x4 & view, const ofMatrix4x4 & projection);
+
+		shared_ptr<ofxMachineVision::Grabber::Simple> getGrabber();
+
+		float getGrabberFrameRate() const;
+
+	protected:
+		shared_ptr<ofxMachineVision::Grabber::Simple> grabber;
+
+		ComboFrame comboFrame;
+
+		ofxSquashBuddies::Utils::FramerateCounter grabberFrameRateCounter;
 	};
 }
