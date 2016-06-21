4f166540418b26e217cc02efb25b8eec3fc70a5d
Elie Zananiri <prisonerjohn@gmail.com>
2016-06-21 10:02:19 -0400

    Added all classes back in lib now that Sender/Receiver and Publisher/Subscriber have made peace.

--- src/ofxMultiTrack.h
+++ src/ofxMultiTrack.h
@@ -2,5 +2,7 @@
 
 #include "ofxMultiTrack/Constants.h"
 #include "ofxMultiTrack/Frame.h"
+#include "ofxMultiTrack/Sender.h"
+#include "ofxMultiTrack/Receiver.h"
 #include "ofxMultiTrack/Publisher.h"
 #include "ofxMultiTrack/Subscriber.h"
